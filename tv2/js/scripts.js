3dc739a30aedc485fbb882cd5afb49aa5bf08305
albi-art <albert.12vi@gmail.com>
2022-07-02 21:28:40 +0300

    moved /tv2 js from index.html to scripts.js

--- tv2/js/scripts.js
+++ tv2/js/scripts.js
@@ -5,3 +5,18 @@ function correctVideoPlayerHeight() {
 
 window.onresize = correctVideoPlayerHeight;
 correctVideoPlayerHeight();
+
+let apiUrl = 'https://albertsongs.asuscomm.com';
+let receiverId = localStorage.getItem('receiverId');
+let changeReceiverIdHandler = (receiverId) => localStorage.setItem('receiverId', receiverId);
+let changePlayerVolumeHandler = (volume) => localStorage.setItem('volume', volume);
+
+let multiPlayer = new MultiPlayer(
+    document.getElementById('player'),
+    document.getElementById('iframePlayer'),
+    localStorage.getItem('volume') ?? 1,
+    changePlayerVolumeHandler
+);
+
+let app = new App(apiUrl, receiverId, multiPlayer, changeReceiverIdHandler);
+app.registerReceiver();
