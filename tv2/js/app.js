fb860c7d0c2f7779749a8c39bad95afae481214d
albi-art <albert.12vi@gmail.com>
2022-07-02 21:36:42 +0300

    reset to 3e23b7a5 (before WebView debug)

--- tv2/js/app.js
+++ tv2/js/app.js
@@ -10,17 +10,19 @@ class App {
     }
 
     messageHandler(mess) {
-        console.log(mess);
+        debug(mess);
         const messContainer = JSON.parse(mess.body);
         const command = JSON.parse(messContainer.message);
         if(command === null || command === undefined) {
             console.error("command is not defined");
+            debug("command is not defined");
             return;
         }
         this.multiPlayer.handleCommand(command);
     }
 
     connectToWebSocket() {
+        debug("connectToWebSocket - receiverId: + " + this.receiverId);
         const receiverId = this.receiverId;
         const CHANEL_PATTERN = '/user/%userId%/private';
         let sock = new SockJS(this.apiUrl + '/ws');
@@ -33,16 +35,18 @@ class App {
                 status: "JOIN"
             };
             stompClient.send('/app/message', {}, JSON.stringify(message));
-        }, (err) => console.log(err));
+        }, (err) => debug(err));
     }
 
     registerReceiver() {
+        debug("registerReceiver - receiverId: + " + this.receiverId);
         this.receiverId == null
             ? this.createReceiver()
             : this.updateReceiver();
     }
 
     createReceiver() {
+        debug("createReceiver - receiverId: + " + this.receiverId);
         const xHttp = new XMLHttpRequest();
         const receiverControllerPath = '/api/v1/receivers';
         const url = this.apiUrl + receiverControllerPath;
@@ -52,7 +56,7 @@ class App {
         };
         xHttp.onreadystatechange = function () {
             if (this.readyState === 4 && this.status === 200) {
-                console.log(xHttp.responseText);
+                debug(xHttp.responseText);
                 let receiver = JSON.parse(xHttp.responseText);
                 that.receiverId = receiver.id;
                 that.changeReceiverIdHandler(that.receiverId);
@@ -66,6 +70,7 @@ class App {
     }
 
     updateReceiver() {
+        debug("updateReceiver - receiverId: + " + this.receiverId);
         const xHttp = new XMLHttpRequest();
         const receiverControllerPath = '/api/v1/receivers/' + this.receiverId;
         const url = this.apiUrl + receiverControllerPath;
@@ -78,11 +83,12 @@ class App {
                 return;
             }
             if (this.status === 200) {
-                console.log(xHttp.responseText);
+                debug(xHttp.responseText);
                 that.connectToWebSocket();
                 that.loadVideos();
             }
             else if ([400,404].includes(this.status)){
+                debug(xHttp.responseText);
                 that.receiverId = null;
                 that.changeReceiverIdHandler(that.receiverId);
                 that.createReceiver();
@@ -94,12 +100,14 @@ class App {
     }
 
     loadVideos() {
+        debug("loadVideos - receiverId: + " + this.receiverId);
         const xHttp = new XMLHttpRequest();
         const receiverControllerPath = "/api/v1/videos";
         const url = this.apiUrl + receiverControllerPath;
         let that = this;
         xHttp.onreadystatechange = function () {
             if (this.readyState === 4 && this.status === 200) {
+                debug(xHttp.responseText);
                 const response = JSON.parse(xHttp.responseText);
                 that.multiPlayer.setVideos(response.list);
             }
