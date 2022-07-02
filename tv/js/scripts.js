174385a7896ba3865b6b5e9a33add7d3f0af6066
albi-art <albert.12vi@gmail.com>
2022-07-03 01:23:05 +0300

    moved tv to repo https://github.com/albertsongs/tv.git

--- tv/js/scripts.js
+++
@@ -1,7 +0,0 @@
-function correctVideoPlayerHeight() {
-    let videoPlayer = document.getElementById("videoPlayer");
-    videoPlayer.style.setProperty("height", window.innerHeight + "px");
-}
-
-window.onresize = correctVideoPlayerHeight;
-correctVideoPlayerHeight();
